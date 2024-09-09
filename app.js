4dc80b72699d7708f4bb2ece63a68fb26ebc0041
Cbstray <calebstraily01@gmail.com>
2024-09-09 16:19:19 -0500

    To Do List Functionality
    Added the ability for a user to enter tasks into a to do list.
    
    The user can click a checkbox next to each task to mark it as complete.
    
    The user can also remove tasks starting from the most recent task added.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let taskList = document.getElementById("taskList");
+let taskList = document.getElementById("taskList").getElementsByTagName('tbody')[0];
+let taskUserInput = document.getElementById("taskUserInput");
 let addTaskButton = document.getElementById("addTaskRow");
 let removeTaskButton = document.getElementById("removeTaskRow");
 
@@ -6,17 +7,75 @@ let taskCount = 1;
 
 addTaskButton.addEventListener("click", () =>
 {
-    let newTaskRow = document.createElement("th");
-    newTaskRow.id = `task-row-${taskCount}`;
-
-    newTaskRow.innerHTML = `<tr>
-                                <th>
-                                    <input type="checkbox">
-                                    <input type="text" placeholder="Insert Task Here">
-                                </th>
-                            </tr>`;            
-    taskCount++;
-
-    taskList.appendChild(newTaskRow);
-
+    addTask();
 });
+
+removeTaskButton.addEventListener("click", () =>
+{
+    removeTask();
+});
+
+function addTask()
+{
+    if (taskUserInput.value != "")
+    {
+        
+        let newRow = taskList.insertRow();
+        newRow.id = `task-row-${taskCount}`;
+        let newCell = newRow.insertCell();
+        let newTaskRow = document.createElement("th");
+        newTaskRow.innerHTML = `<tr>
+                                    <h2 class="taskHeader">
+                                        <input class="checkBox" id="taskCheckBox-${taskCount}" type="checkbox" onclick="toggleTask(this)">
+                                        Task ${taskCount}:
+                                    </h2>
+                                    <p>
+                                        <input class="taskValue" type="text" value="${taskUserInput.value}">
+                                    </p>
+                                </tr>`;
+        taskCount++;
+        newCell.appendChild(newTaskRow);
+        taskUserInput.value = "";
+    }
+}
+
+function removeTask()
+{
+    let lastChild = taskList.lastElementChild;
+    let rowCheck = document.getElementById("inputRow");
+
+    if (lastChild != rowCheck)
+    {
+        taskList.removeChild(lastChild);
+        taskCount--;
+    }
+}
+
+function toggleTask(checkbox)
+{
+    let taskList = document.querySelectorAll(".taskValue");
+    let taskHeaders = document.querySelectorAll(".taskHeader");
+
+    for (let i = 0; i < taskList.length; i++)
+    {
+        let checkBoxIdSplit = checkbox.id.split('-');
+
+        let numberCheck = parseInt(checkBoxIdSplit[1]);
+
+        if ((i + 1) == numberCheck)
+        {
+            if (checkbox.checked)
+            {
+                taskList[i].classList.add('check-box-crossout');
+                taskHeaders[i].classList.add('header-crossout');
+            }
+            else
+            {
+                taskList[i].classList.remove('check-box-crossout');
+                taskHeaders[i].classList.remove('header-crossout');
+            }
+
+            return;
+        }
+    }
+}
